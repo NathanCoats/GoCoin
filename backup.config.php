42126f6d1d01ba3b16fee7f7719238400d38f2f0
coanat40 <natec@plumbersstock.com>
2018-01-10 17:56:11 -0700

    removing unneeded code, for now

--- backup.config.php
+++ backup.config.php
@@ -25,72 +25,56 @@
 				"high_percentage" => 1.0,
 				"low_percentage" => 1.5,
 				'time_limits' => [
-					(object)[ "minutes"	=> .2, "percent"	=> .5 ],
-					//(object)[ "minutes"	=> 2, "percent"	=> 2.0 ],
-					//(object)[ "minutes"	=> 5, "percent"	=> 3.0 ]
+					(object)[ "minutes"	=> .2, "percent"	=> .5 ]
 				]
 			],
 			"btc-dash" => (object)[
 				"high_percentage" => .75,
 				"low_percentage" => 2.0,
 				'time_limits' => [
-					(object)[ "minutes"	=> .2, "percent"	=> .5 ],
-					//(object)[ "minutes"	=> 2, "percent"	=> 2.0 ],
-					//(object)[ "minutes"	=> 5, "percent"	=> 3.0 ]
+					(object)[ "minutes"	=> .2, "percent"	=> .5 ]
 				]
 			],
 			"btc-ark" => (object)[
 				"high_percentage" => .75,
 				"low_percentage" => 3.0,
 				'time_limits' => [
-					(object)[ "minutes"	=> .2, "percent"	=> .5 ],
-					//(object)[ "minutes"	=> 2, "percent"	=> 2.0 ],
-					//(object)[ "minutes"	=> 5, "percent"	=> 3.0 ]
+					(object)[ "minutes"	=> .2, "percent"	=> .5 ]
 				]
 			],
 			"btc-powr" => (object)[
 				"high_percentage" => .75,
 				"low_percentage" => 2.0,
 				'time_limits' => [
-					(object)[ "minutes"	=> .2, "percent"	=> .5 ],
-					//(object)[ "minutes"	=> 2, "percent"	=> 2.0 ],
-					//(object)[ "minutes"	=> 5, "percent"	=> 3.0 ]
+					(object)[ "minutes"	=> .2, "percent"	=> .5 ]
 				]
 			],
 			"btc-eth" => (object)[
 				"high_percentage" => 2.0,
 				"low_percentage" => 2.0,
 				'time_limits' => [
-					(object)[ "minutes"	=> .2, "percent"	=> 2.0 ],
-					//(object)[ "minutes"	=> 2, "percent"	=> 2.0 ],
-					//(object)[ "minutes"	=> 5, "percent"	=> 3.0 ]
+					(object)[ "minutes"	=> .2, "percent"	=> 2.0 ]
 				]
 			],
 			"btc-ltc" => (object)[
 				"high_percentage" => .75,
 				"low_percentage" => 2.0,
 				'time_limits' => [
-					(object)[ "minutes"	=> .2, "percent"	=> .5 ],
-					//(object)[ "minutes"	=> 2, "percent"	=> 2.0 ],
-					//(object)[ "minutes"	=> 5, "percent"	=> 3.0 ]
+					(object)[ "minutes"	=> .2, "percent"	=> .5 ]
 				]
 			],
 			"btc-vtc" => (object)[
 				"high_percentage" => 1.0,
 				"low_percentage" => 2.0,
 				'time_limits' => [
-					(object)[ "minutes"	=> .2, "percent"	=> 1.5 ],
-					//(object)[ "minutes"	=> 2, "percent"	=> 2.0 ],
-					//(object)[ "minutes"	=> 5, "percent"	=> 3.0 ]
+					(object)[ "minutes"	=> .2, "percent"	=> 1.5 ]
 				]
 			],
 			"btc-waves" => (object)[
 				"high_percentage" => 1.0,
 				"low_percentage" => 1.0,
 				'time_limits' => [
-					(object)[ "minutes"	=> .2, "percent"	=> .5 ],
-					//(object)[ "minutes"	=> 2, "percent"	=> 2.0 ],
-					//(object)[ "minutes"	=> 5, "percent"	=> 3.0 ]
+					(object)[ "minutes"	=> .2, "percent"	=> .5 ]
 				]
 			],
 			"btc-mona" => (object)[
@@ -98,17 +82,13 @@
 				"low_percentage" => 2.0,
 				'time_limits' => [
 					(object)[ "minutes"	=> .2, "percent"	=> .5 ],
-					//(object)[ "minutes"	=> 2, "percent"	=> 2.0 ],
-					//(object)[ "minutes"	=> 5, "percent"	=> 3.0 ],
 				]
 			],
 			"btc-omg" => (object)[
 				"high_percentage" => 1.5,
 				"low_percentage" => 2.0,
 				'time_limits' => [
-					(object)[ "minutes"	=> .2, "percent"	=> .5 ],
-					//(object)[ "minutes"	=> 2, "percent"	=> 2.0 ],
-					//(object)[ "minutes"	=> 5, "percent"	=> 3.0 ]
+					(object)[ "minutes"	=> .2, "percent"	=> .5 ]
 				]
 			],
 			// "btc-eng" => (object)[
@@ -124,9 +104,7 @@
 				"high_percentage" => .75,
 				"low_percentage" => 1.0,
 				'time_limits' => [
-					(object)[ "minutes"	=> .2, "percent"	=> .5 ],
-					//(object)[ "minutes"	=> 2, "percent"	=> 2.0 ],
-					//(object)[ "minutes"	=> 5, "percent"	=> 3.0 ]
+					(object)[ "minutes"	=> .2, "percent"	=> .5 ]
 				]
 			],
 			// "btc-nxt" => (object)[
@@ -142,18 +120,14 @@
 				"high_percentage" => .75,
 				"low_percentage" => 2.0,
 				'time_limits' => [
-					(object)[ "minutes"	=> .2, "percent"	=> .5 ],
-					//(object)[ "minutes"	=> 2, "percent"	=> 2.0 ],
-					//(object)[ "minutes"	=> 5, "percent"	=> 3.0 ]
+					(object)[ "minutes"	=> .2, "percent"	=> .5 ]
 				]
 			],
 			"btc-snt" => (object)[
 				"high_percentage" => 1.5,
 				"low_percentage" => 2.0,
 				'time_limits' => [
-					(object)[ "minutes"	=> .2, "percent"	=> .5 ],
-					//(object)[ "minutes"	=> 2, "percent"	=> 2.0 ],
-					//(object)[ "minutes"	=> 5, "percent"	=> 3.0 ]
+					(object)[ "minutes"	=> .2, "percent"	=> .5 ]
 				]
 			],
 			"btc-emc2" => (object)[
@@ -178,18 +152,14 @@
 				"high_percentage" => 1.75,
 				"low_percentage" => 2.0,
 				'time_limits' => [
-					(object)[ "minutes"	=> .2, "percent"	=> .5 ],
-					//(object)[ "minutes"	=> 2, "percent"	=> 2.0 ],
-					//(object)[ "minutes"	=> 5, "percent"	=> 3.0 ]
+					(object)[ "minutes"	=> .2, "percent"	=> .5 ]
 				]
 			],
 			"btc-xlm" => (object)[
 				"high_percentage" => .75,
 				"low_percentage" => 3.0,
 				'time_limits' => [
-					(object)[ "minutes"	=> .2, "percent"	=> .5 ],
-					//(object)[ "minutes"	=> 2, "percent"	=> 2.0 ],
-					//(object)[ "minutes"	=> 5, "percent"	=> 3.0 ]
+					(object)[ "minutes"	=> .2, "percent"	=> .5 ]
 				]
 			],
 		],
